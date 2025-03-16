2d91f605e34e1c89163f6ae947b1400a929cfff7
kinchungwong <kinchungwong@users.noreply.github.com>
2025-03-16 01:43:18 -0700

    download_url improvement
    1. prints if overwriting
    2. auto create parent dir

--- intel_simd_knowledge_toy/data_tools/network/download_url.py
+++ intel_simd_knowledge_toy/data_tools/network/download_url.py
@@ -4,11 +4,16 @@ from pathlib import Path
 from intel_simd_knowledge_toy.data_tools.network._utils import _DownloadProgress
 
 def download_url(url: str, target_file: Union[str, Path]):
-    target_file = str(Path(target_file))
+    target_file_p = Path(target_file)
+    target_file_s = str(target_file_p)
+    is_overwriting = target_file_p.exists()
+    if not target_file_p.parent.exists():
+        target_file_p.parent.mkdir(parents=True)
+    download_or_overwrite_s = "Overwriting: " if is_overwriting else "Downloading to: "
     print("Download from: " + url)
-    print("Download to: " + target_file)
+    print(download_or_overwrite_s + target_file_s)
     progress = _DownloadProgress(print)
-    urllib.request.urlretrieve(url, target_file, reporthook=progress)
+    urllib.request.urlretrieve(url, target_file_s, reporthook=progress)
     print("Download complete.")
 
 if __name__ == "__main__":
